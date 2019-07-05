003a3020cbf63ba89084e0b56592c0c269148a61
haoda <haoda@bolo.me>
2019-07-05 16:05:08 +0800

    add Merge

--- operator.go
+++ operator.go
@@ -1,5 +1,7 @@
 package reactive_channel
 
+import "sync"
+
 func From(in []interface{}) chan interface{} {
 	out := make(chan interface{}, len(in))
 
@@ -59,6 +61,38 @@ func Filter(in <- chan interface{}, filterFunc FilterFunc) chan interface{} {
 			}
 		}
 	}()
+
+	return out
+}
+
+func Merge(chans ... chan interface{}) chan interface{} {
+	out := make(chan interface{})
+
+	mutex := sync.Mutex{}
+	closedCount := 0
+
+	for _, c := range chans {
+		ch := c
+
+		go func() {
+			for {
+				val, ok := <- ch
+				if ok {
+					out <- val
+				} else {
+					mutex.Lock()
+					closedCount++
+
+					if closedCount == len(chans) {
+						close(out)
+					}
+
+					mutex.Unlock()
+					return
+				}
+			}
+		}()
+	}
 
 	return out
 }
--- operator_test.go
+++ operator_test.go
@@ -1,10 +1,62 @@
 package reactive_channel
 
-import "testing"
+import (
+	"sort"
+	"testing"
+)
 
-func isArrayEqual(v1[]interface{}, v2[]interface{}) bool {
-	if len(v1) != len(v1) {
+type SortInterfaceArray []interface{}
+
+func (s SortInterfaceArray) Len() int {
+	return len(s)
+}
+
+func (s SortInterfaceArray) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s SortInterfaceArray) Less(i, j int) bool {
+	switch s[i].(type) {
+	case int:
+		return s[i].(int) < s[j].(int)
+	case uint:
+		return s[i].(uint) < s[j].(uint)
+	case int8:
+		return s[i].(int8) < s[j].(int8)
+	case int16:
+		return s[i].(int16) < s[j].(int16)
+	case int32:
+		return s[i].(int32) < s[j].(int32)
+	case int64:
+		return s[i].(int64) < s[j].(int64)
+	case uint8:
+		return s[i].(uint8) < s[j].(uint8)
+	case uint16:
+		return s[i].(uint16) < s[j].(uint16)
+	case uint32:
+		return s[i].(uint32) < s[j].(uint32)
+	case uint64:
+		return s[i].(uint64) < s[j].(uint64)
+	case float32:
+		return s[i].(float32) < s[j].(float32)
+	case float64:
+		return s[i].(float64) < s[j].(float64)
+	case string:
+		return s[i].(string) < s[j].(string)
+	default:
+		return true
+	}
+}
+
+func isArrayEqual(v1 SortInterfaceArray, v2 SortInterfaceArray, strict bool) bool {
+	if len(v1) != len(v2) {
 		return false
+	}
+
+	if ! strict {
+		sort.Sort(v1)
+		sort.Sort(v2)
+
 	}
 
 	for index, v := range v1{
@@ -18,7 +70,7 @@ func isArrayEqual(v1[]interface{}, v2[]interface{}) bool {
 
 func assertChanWithValues(t *testing.T, c chan interface{}, vals []interface{}) {
 	inVals := To(c)
-	if !isArrayEqual(inVals, vals) {
+	if !isArrayEqual(inVals, vals, true) {
 		t.Errorf("assertChanWithValues fail, chan: %v, array: %v", inVals, vals)
 	}
 }
@@ -28,7 +80,7 @@ func TestFromTo(t *testing.T) {
 	c := From(in)
 	out := To(c)
 
-	if !isArrayEqual(in, out) {
+	if !isArrayEqual(in, out, true) {
 		t.Error("From or To failed")
 	}
 }
@@ -53,4 +105,18 @@ func TestFilter(t *testing.T) {
 	})
 
 	assertChanWithValues(t, out, []interface{}{2, 4})
+}
+
+func TestMerge(t *testing.T) {
+	c1 := From([]interface{}{1,2,9})
+	c2 := From([]interface{}{3,4,10})
+	c3 := From([]interface{}{5,7,11})
+	c4 := From([]interface{}{6,8,12})
+
+	ch := Merge(c1, c2, c3, c4)
+
+	array := To(ch)
+	if !isArrayEqual(array, []interface{}{1,2,3,4,5,6,7,8,9,10,11,12}, false) {
+		t.Error("merge values not equal to original ones")
+	}
 }
